74ec3d077499b28605620a0ec8eb9dfd34a1d211
Raiyyan <inforaiyyan@gmail.com>
2020-12-21 20:31:19 +0530

    Updates

--- src/Header.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Header = ({length}) => {
-    return (<h4 className="bg-warning text-center text-white p-3  ">Todo List ({length} to do items)</h4>
-    );
-}
-
-export default Header;
